c73ef3b58e682dbb58339d3c11eb04d64e92d84e
Skyler Bala <skyler.bala@gmail.com>
2020-06-02 14:11:16 -0700

    added scripts to create network and client

--- src/Client.py
+++ src/Client.py
@@ -93,6 +93,7 @@ class Client:
             self.sock.sendall(str.encode(str(setupMsg)))
 
             buffer = self.sock.recv(32768).decode("utf-8")
+            print(buffer)
             print('gang')
             self.chainServersPublicKeys = pickle.load(buffer)
             print(self.chainServersPublicKeys)
